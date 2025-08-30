9f4ef9466307fccd40bd1bfd557d52eb1bfcebf7
thisisnitintiwarii <thisisnitintiwari@gmail.com>
2025-08-31 01:25:44 +0530

    util commits

---
+++ src/index.js
@@ -0,0 +1,51 @@
+// require('dotenv').config({path : './env'})
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+    .then(()=>{
+        app.listen(process.env.PORT || 8000, ()=>{
+            console.log(`mogo db running on ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log("MONGO db connection failed !!!", err)
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const app = express()
+
+(async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI} / ${DB_NAME}`);
+        app.on("error",(error)=>{
+            console.log("not listen");
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`app is listining ${process.env.PORT}`);
+        })
+    }
+    catch(error){
+        console.log("ERROR :",)
+    }
+})()
+
+*/
